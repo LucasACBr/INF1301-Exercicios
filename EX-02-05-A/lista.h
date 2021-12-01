b3932388d0477c98520b63085a2dbae149dcaa24
LucasACBr <lucas@projetoacbr.com.br>
2021-12-01 14:41:11 -0300

    Create lista.h

---
+++ EX-02-05-A/lista.h
@@ -0,0 +1,25 @@
+//Lucas Simões de Almeida - Matrícula 1712101
+
+typedef struct lista Lista;
+
+Lista * lst_cria(void);
+
+int lst_vazia(Lista* lst);
+
+void lst_insIni(Lista * lst, void * cod);
+
+void lst_insFin(Lista * lst, void * cod);
+
+void *lst_retIni(Lista * lst);
+
+void *lst_retFin(Lista * lst);
+
+void lst_posIni(Lista * lst);
+
+void lst_posFin(Lista * lst);
+
+void *lst_prox(Lista * lst);
+
+void *lst_ant(Lista * lst);
+
+void lst_libera(Lista * lst);
