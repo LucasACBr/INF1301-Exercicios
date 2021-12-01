5439f347705d47eb3743d51b7ab43248ec024411
LucasACBr <lucas@projetoacbr.com.br>
2021-12-01 14:41:21 -0300

    Create pilha.h

---
+++ EX-02-04-A/pilha.h
@@ -0,0 +1,13 @@
+//Lucas Simoes de Almeida - 1712101
+
+typedef struct pilha Pilha;
+
+Pilha *pilha_cria(void);
+
+int pilha_vazia(Pilha *p);
+
+void pilha_push(Pilha* p, void *v);
+
+void *pilha_pop(Pilha* p);
+
+void pilha_libera(Pilha *p);
